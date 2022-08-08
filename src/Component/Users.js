d704aca3b7c63f69dc5291ca86e23301571eabf0
Your Name <pooja.powar@ltts.com>
2022-08-08 15:31:15 +0530

    first commit message

---
+++ src/Component/Users.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+const Users = [
+    {
+      id: 1,
+      selected: false,
+      name: "Leanne Graham",
+      email: "Sincere@april.biz",
+    },
+    {
+      id: 2,
+      selected: false,
+      name: "Ervin Howell",
+      email: "Shanna@melissa.tv",
+    },
+    {
+      id: 3,
+      selected: false,
+      name: "Clementine Bauch",
+      email: "Nathan@yesenia.net",
+    },
+    {
+      id: 4,
+      selected: false,
+      name: "Patricia Lebsack",
+      email: "Julianne.OConner@kory.org",
+    },
+    {
+      id: 5,
+      selected: false,
+      name: "Chelsey Dietrich",
+      email: "Lucio_Hettinger@annie.ca",
+    },
+  ];
+  
+  export default Users;
